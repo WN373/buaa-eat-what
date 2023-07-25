c3291885ea06b38085ee2faeba955b26c9e15b38
Xjchen <wnico233@outlook.com>
2023-07-26 01:24:46 +0800

    gpt assisted auth

---
+++ backend/eat_back/login/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+# GPT assisted
+
+@csrf_exempt
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return JsonResponse({'code': 200, 'msg': '登录成功'})
+        else:
+            errors = dict(form.errors.items())
+            return JsonResponse({'code': 400, 'msg': '登录失败', 'errors': errors})
+    else:
+        form = AuthenticationForm()
+    return JsonResponse({'code': 400, 'msg': '请求方式错误'})
+
+@csrf_exempt
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return JsonResponse({'code': 200, 'msg': '注册成功'})
+        else:
+            errors = dict(form.errors.items())
+            return JsonResponse({'code': 400, 'msg': '注册失败', 'errors': errors})
+    else:
+        form = UserCreationForm()
+    return JsonResponse({'code': 400, 'msg': '请求方式错误'})
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+def index(request):
+    return HttpResponse('主页面')
